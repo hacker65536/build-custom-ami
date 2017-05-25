f9aaa0a7fbde9dd68a8c6de04722446ad2437caa
hacker65536 <s.hacker65536@gmail.com>
2017-05-25 09:06:10 +0000

    Fix

--- codepipeline.tf
+++ codepipeline.tf
@@ -1,11 +1,22 @@
 # vim:set foldmethod=marker:
 
+resource "aws_iam_role" "cpl" {
+  name               = "${terraform.env}-cpl-role"
+  assume_role_policy = "${data.aws_iam_policy_document.clp_srv_role_assume.json}"
+}
+
+resource "aws_iam_role_policy" "cpl_policy" {
+  role   = "${aws_iam_role.cpl.id}"
+  policy = "${data.aws_iam_policy_document.clp_srv_role_policy.json}"
+}
+
 resource "aws_codepipeline" "cpl" {
   name     = "${terraform.env}-cpl"
   role_arn = "${aws_iam_role.cpl.arn}"
 
   artifact_store {
     location = "${aws_s3_bucket.s3.bucket}"
+    type     = "S3"
   }
 
   stage {
@@ -20,10 +31,29 @@ resource "aws_codepipeline" "cpl" {
       output_artifacts = ["myapp"]
 
       configuration {
-        owner      = "techadmin"
-        Repo       = "${var.github_repo}"
-        Branch     = "${var.github_branch}"
-        OAuthToken = "${var.github_token}"
+        Owner      = "${lookup(var.github,"owner")}"
+        Repo       = "${lookup(var.github,"repo")}"
+        Branch     = "${lookup(var.github,"branch")}"
+        OAuthToken = "${lookup(var.github,"token")}"
+      }
+    }
+  }
+
+  stage {
+    name = "Build"
+
+    action {
+      name     = "Build"
+      category = "Build"
+      owner    = "AWS"
+      provider = "CodeBuild"
+      version  = "1"
+
+      input_artifacts  = ["myapp"]
+      output_artifacts = ["myappbuild"]
+
+      configuration {
+        ProjectName = "${aws_codebuild_project.cb.name}"
       }
     }
   }
